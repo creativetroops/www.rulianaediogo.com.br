8c878c04f729b0921842d34a244ce44175561efc
Diogo Cezar <diogo@diogocezar.com>
2019-05-09 20:32:05 -0300

    Updates

--- precache-manifest.e938f971afb6437e3d506d1b4ada3f3c.js
+++ precache-manifest.e938f971afb6437e3d506d1b4ada3f3c.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "c081f0adfeb04e269f79",
-    "url": "./static/js/main.c081f0ad.chunk.js"
+    "revision": "7ea86ed01e51648b91f1",
+    "url": "./static/js/main.7ea86ed0.chunk.js"
   },
   {
     "revision": "51ffd079c5a1a6fa6765",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "bac4e7121e6973e40d76e944d347b94c",
+    "revision": "a5d1cbcd2fb3f9fa556f7db737b332e1",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1c050148ee586d82c339358753321487.js"
+  "./precache-manifest.e938f971afb6437e3d506d1b4ada3f3c.js"
 );
 
 workbox.clientsClaim();
