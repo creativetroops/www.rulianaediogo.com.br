c8f2c9e023696aeca2678bb14552a412cbf0898f
Diogo Cezar <diogo@diogocezar.com>
2019-05-08 23:03:13 -0300

    General Implementations

--- back-end/src/controllers/SendMailController/index.js
+++ back-end/src/controllers/SendMailController/index.js
@@ -56,23 +56,23 @@ class SendMailController {
 
   getStatus(status) {
     switch (status) {
-      case 1:
+      case '1':
         return 'Aguardando pagamento'
-      case 2:
+      case '2':
         return 'Em análise'
-      case 3:
+      case '3':
         return 'Paga'
-      case 4:
+      case '4':
         return 'Disponível'
-      case 5:
+      case '5':
         return 'Em disputa'
-      case 6:
+      case '6':
         return 'Devolvida'
-      case 7:
+      case '7':
         return 'Cancelada'
-      case 8:
+      case '8':
         return 'Debitado'
-      case 9:
+      case '9':
         return 'Retenção temporária'
       default:
         return 'Não informado'
